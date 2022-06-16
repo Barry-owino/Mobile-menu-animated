b109fd5690b406ec618916b8d6b5d80601c46176
Barry-owino <barryowino5@gmail.com>
2022-06-16 07:32:16 +0300

    added the javascript remove and add functionality

---
+++ app.js
@@ -0,0 +1,14 @@
+let links = document.getElementsByClassName("link");
+
+function removeActive(){
+  for (link of links){
+    link.classList.remove("active");
+  }
+}
+
+for (link of links){
+  link.onclick = function () {
+    removeActive();
+    this.classList.add("active");
+  }
+}
